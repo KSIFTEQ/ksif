2f2d51a0afcb2b38713c717a2a66abaa9ccf5616
Sangheum Cho <sh.cho@kaist.ac.kr>
2020-02-29 21:18:14 +0900

    New distribution

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(
-    name='ksif2',
+    name='ksif',
     version='2020.2.29',
     description='Quantitative investment tools for KSIF',
     long_description=long_description,
